7e74b231389362f06bd06694f77a3bbf4dfcb168
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-15 08:02:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,7 +34,7 @@ class TicTacToe
 
   def turn_count
     count = 0
-    board.each do |position|
+    @board.each do |position|
       if position == "X" || position == "O"
         count += 1
       end
